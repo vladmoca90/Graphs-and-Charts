e88a3baff580d0cd3bbae1b9597b53735707f89e
Vlad Mocanu <vladmoca90@gmail.com>
2018-08-11 14:06:45 +0100

    Created a range graph with UK temperature by month in 2017

--- scripts/temperature-2.js
+++ scripts/temperature-2.js
@@ -1,50 +1,73 @@
 window.onload = function () {
-    var chart = new CanvasJS.Chart("temperature2", {
+    var chart = new CanvasJS.Chart('temperature2', {
         title: {
-            text: "Delhi Temperature (1 Apr - 7 Apr) 2014"
+            margin: 20,
+            text: 'UK Temperature (1 Jan - 31 Dec) 2017',
         },
         axisY: {
-            includeZero: false,
-            interval: 2,
-            valueFormatString: "#0.## °C"
+            includeZero: true,
+            interval: 3,
+            valueFormatString: '#0.## °C'
         },
         axisX: {
             interval: 1,
-            valueFormatString: "DD-MMM"
+            valueFormatString: 'DD-MMM'
         },
         data: [{
-            type: "rangeBar",
-            yValueFormatString: "#0.## °C",
-            dataPoints: [{
-                    x: new Date(2014, 03, 1),
-                    y: [17, 33]
+            type: 'rangeBar',
+            yValueFormatString: '#0.## °C',
+            dataPoints: [
+                {
+                    x: new Date(2017, 00, 1),
+                    y: [2, 10]
                 },
                 {
-                    x: new Date(2014, 03, 2),
-                    y: [18, 35]
+                    x: new Date(2017, 01, 1),
+                    y: [1, 8]
                 },
                 {
-                    x: new Date(2014, 03, 3),
-                    y: [18, 32]
+                    x: new Date(2017, 02, 1),
+                    y: [4, 11]
                 },
                 {
-                    x: new Date(2014, 03, 4),
-                    y: [18, 32]
+                    x: new Date(2017, 03, 1),
+                    y: [7, 15]
                 },
                 {
-                    x: new Date(2014, 03, 5),
-                    y: [20, 35]
+                    x: new Date(2017, 04, 1),
+                    y: [10, 17]
                 },
                 {
-                    x: new Date(2014, 03, 6),
-                    y: [20, 38]
+                    x: new Date(2017, 05, 1),
+                    y: [14, 21]
                 },
                 {
-                    x: new Date(2014, 03, 7),
-                    y: [21, 32]
+                    x: new Date(2017, 06, 1),
+                    y: [14, 24]
+                },
+                {
+                    x: new Date(2017, 07, 1),
+                    y: [18, 27]
+                },
+                {
+                    x: new Date(2017, 08, 1),
+                    y: [10, 20]
+                },
+                {
+                    x: new Date(2017, 09, 1),
+                    y: [9, 16]
+                },
+                {
+                    x: new Date(2017, 10, 1),
+                    y: [9, 16]
+                },
+                {
+                    x: new Date(2017, 11, 1),
+                    y: [2, 12]
                 }
             ]
         }]
     });
+
     chart.render();
 }
