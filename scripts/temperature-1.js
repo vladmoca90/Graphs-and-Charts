995b13aabc772bef8ad33489730dcfcf9abf7768
Vlad Mocanu <vladmoca90@gmail.com>
2018-08-11 15:43:54 +0100

    Created a chart with the monthly average temperatures from Astana

--- scripts/temperature-1.js
+++ scripts/temperature-1.js
@@ -1,68 +1,89 @@
 window.onload = function () {
+    CanvasJS.addColorSet('monthColor', [
+        '#1A237E',
+        '#3949AB',
+        '#00BCD4',
+        '#4CAF50',
+        '#AFB42B',
+        '#FFEB3B',
+        '#FF8F00',
+        '#F4511E',
+        '#E57373',
+        '#9C27B0',
+        '#9575CD',
+        '#3F51B5',
+    ]);
+
     var chart = new CanvasJS.Chart('temperature1', {
+        animationEnabled: true,
         title: {
-            text: 'Basic Range Column Chart'
+            fontSize: 18,
+            margin: 25,
+            text: 'Climate Data for Astana, Kazakhstan (Average High and Low)',
         },
         axisY: {
+            gridColor: '#ddd',
             includeZero: false,
-            interval: 10,
+            interval: 7,
             title: 'Temperature (C)'
         },
         axisX: {
             interval: 1,
             intervalType: 'month',
-            valueFormatString: 'MMM'
+            title: 'Month',
+            valueFormatString: 'MMM',
         },
         data: [{
             type: 'rangeColumn',
-            color: '#369EAD',
+            colorSet: 'colorMonth',
+            fillOpacity: .85,
             dataPoints: [{
                     x: new Date(2012, 00, 01),
-                    y: [-9.7, 10.4]
+                    y: [-18.3, -9.9]
                 }, // y: [Low ,High]
                 {
                     x: new Date(2012, 01, 01),
-                    y: [-8.7, 16.5]
+                    y: [-18.5, -9.2]
                 },
                 {
                     x: new Date(2012, 02, 01),
-                    y: [-3.5, 19.4]
+                    y: [-11.5, -2.5]
                 },
                 {
                     x: new Date(2012, 03, 01),
-                    y: [5.0, 22.6]
+                    y: [0.2, 10.9]
                 },
                 {
                     x: new Date(2012, 04, 01),
-                    y: [7.9, 29.5]
+                    y: [7.9, 20.2]
                 },
                 {
                     x: new Date(2012, 05, 01),
-                    y: [9.2, 34.7]
+                    y: [13.2, 25.8]
                 },
                 {
                     x: new Date(2012, 06, 01),
-                    y: [7.3, 30.5]
+                    y: [15, 26.8]
                 },
                 {
                     x: new Date(2012, 07, 01),
-                    y: [4.4, 25.5]
+                    y: [12.8, 25.2]
                 },
                 {
                     x: new Date(2012, 08, 01),
-                    y: [-3.1, 20.4]
+                    y: [6.6, 18.8]
                 },
                 {
                     x: new Date(2012, 09, 01),
-                    y: [-5.2, 15.4]
+                    y: [0.2, 10]
                 },
                 {
                     x: new Date(2012, 10, 01),
-                    y: [-13.5, 9.8]
+                    y: [-8.9, -1.4]
                 },
                 {
                     x: new Date(2012, 11, 01),
-                    y: [-15.5, 5.8]
+                    y: [-16.1, -8]
                 }
             ]
         }]
