1cb7c40816ff66eea3ea920c2853f7a195cad63b
Vlad Mocanu <vladmoca90@gmail.com>
2019-07-19 23:46:43 +0100

    Changed " into '

--- FusionCharts/oil-reserves/oil-reserves.js
+++ FusionCharts/oil-reserves/oil-reserves.js
@@ -7,39 +7,39 @@ FusionCharts.ready(function () {
         dataFormat: 'json',
         dataSource: {
             // Chart Configuration
-            "chart": {
-                "caption": "Countries With Most Oil Reserves (2017-18)",
-                "subCaption": "In MMbbl (One Million barrels)",
-                "xAxisName": "Country",
-                "yAxisName": "Reserves (MMbbl)",
-                "numberSuffix": "K",
-                "theme": "fusion",
+            'chart': {
+                'caption': 'Countries With Most Oil Reserves (2017-18)',
+                'subCaption': 'In MMbbl (One Million barrels)',
+                'xAxisName': 'Country',
+                'yAxisName': 'Reserves (MMbbl)',
+                'numberSuffix': 'K',
+                'theme': 'fusion',
             },
             // Chart Data
-            "data": [{
-                "label": "Venezuela",
-                "value": "320"
+            'data': [{
+                'label': 'Venezuela',
+                'value': '320'
             }, {
-                "label": "Saudi Arabia",
-                "value": "225"
+                'label': 'Saudi Arabia',
+                'value': '225'
             }, {
-                "label": "Canada",
-                "value": "180"
+                'label': 'Canada',
+                'value': '180'
             }, {
-                "label": "Iran",
-                "value": "150"
+                'label': 'Iran',
+                'value': '150'
             }, {
-                "label": "Russia",
-                "value": "125"
+                'label': 'Russia',
+                'value': '125'
             }, {
-                "label": "UAE",
-                "value": "100"
+                'label': 'UAE',
+                'value': '100'
             }, {
-                "label": "USA",
-                "value": "80"
+                'label': 'USA',
+                'value': '80'
             }, {
-                "label": "PR China",
-                "value": "65"
+                'label': 'PR China',
+                'value': '65'
             }]
         }
     });
