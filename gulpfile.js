98e9c97a1b598a11adaa40856c9ee5124b708643
Vlad Mocanu <vladmoca90@gmail.com>
2020-02-24 22:41:49 +0000

    Changed

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,12 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 
 gulp.task('sass', function () {
-  return gulp.src('projects/**/*.scss')
+  return gulp.src('styles/*.scss')
     .pipe(sass())
-    .pipe(gulp.dest('projects/'))
+    .pipe(gulp.dest('styles/'))
 });
 
 //Watch task
 gulp.task('default', function () {
-  return gulp.watch('projects/**/*.scss', gulp.series('sass'));
+  return gulp.watch('styles/*.scss', gulp.series('sass'));
 });
