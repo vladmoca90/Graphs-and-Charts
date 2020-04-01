ae9a1bdaaac40a546bca53e00d0dcd46ee0ddc1c
Vlad Mocanu <vladmoca90@gmail.com>
2020-04-01 01:35:59 +0100

    Added the data outside the $ function

--- projects/company-revenue/company-revenue.js
+++ projects/company-revenue/company-revenue.js
@@ -1,47 +1,45 @@
-$(function() {
-    var data = [
-        [new Date(2000, 0), '2289000'],
-        [new Date(2001, 0), '1946000'],
-        [new Date(2002, 0), '1270000'],
-        [new Date(2003, 0), '1846000'],
-        [new Date(2004, 0), '2250000'],
-        [new Date(2005, 0), '2222000'],
-        [new Date(2006, 0), '2075000'],
-        [new Date(2007, 0), '2345000'],
-        [new Date(2008, 0), '2097000'],
-        [new Date(2009, 0), '2201000'],
-        [new Date(2010, 0), '2438000'],
-    ];
+var data = [
+    [new Date(2000, 0), '2289000'],
+    [new Date(2001, 0), '1946000'],
+    [new Date(2002, 0), '1270000'],
+    [new Date(2003, 0), '1846000'],
+    [new Date(2004, 0), '2250000'],
+    [new Date(2005, 0), '2222000'],
+    [new Date(2006, 0), '2075000'],
+    [new Date(2007, 0), '2345000'],
+    [new Date(2008, 0), '2097000'],
+    [new Date(2009, 0), '2201000'],
+    [new Date(2010, 0), '2438000'],
+];
 
-     var revenue = [];
+$(function () {
+    var revenue = [];
 
     function showRevenue(data) {
-        if(!data) {
+        if (!data) {
             throw new Error('The list must be given');
         }
-        if(data.length == 0) {
+        if (data.length == 0) {
             throw new Error('The list cannot be empty');
         }
 
-        for(var i = 0; i < data.length; i++) {
+        for (var i = 0; i < data.length; i++) {
             var points = {};
 
             points.x = data[i][0];
             points.y = parseInt(data[i][1]);
-
             revenue.push(points);
         }
 
-       return revenue;
+        return revenue;
     }
 
     showRevenue(data);
 
     window.onload = function () {
-
         var options = {
-            animationEnabled: true,  
-            title:{
+            animationEnabled: true,
+            title: {
                 fontSize: 28,
                 fontWeight: 'normal',
                 margin: 0,
@@ -54,8 +52,7 @@ $(function() {
                 suffix: "mn",
                 prefix: "$"
             },
-            data: [
-            {
+            data: [{
                 type: "area",
                 markerSize: 5,
                 xValueFormatString: "YYYY",
