2b778c35d8e493248863020b63f64b03cfe99314
Vlad Mocanu <vladmoca90@gmail.com>
2020-04-01 22:56:25 +0100

    Working of the data and dates

--- projects/coronavirus/daily-cases-worldwide/daily-cases-worldwide.js
+++ projects/coronavirus/daily-cases-worldwide/daily-cases-worldwide.js
@@ -36,130 +36,55 @@ $(function () {
                 type: "line",
                 xValueFormatString: "DD MMM",
                 color: "#7bd6ed",
-                dataPoints: [{
-                        x: new Date(2017, 0, 1),
-                        y: 610
+                dataPoints: [
+                     {
+                        x: new Date(2020, 0, 1),
+                        y: 0
                     },
                     {
-                        x: new Date(2017, 0, 2),
-                        y: 680
+                        x: new Date(2020, 1, 22),
+                        y: 580
+                    },
+                     {
+                        x: new Date(2020, 1, 25),
+                        y: 6058
                     },
                     {
-                        x: new Date(2017, 0, 3),
-                        y: 690
+                        x: new Date(2020, 2, 1),
+                        y: 14530
                     },
                     {
-                        x: new Date(2017, 0, 4),
-                        y: 700
+                        x: new Date(2020, 2, 7),
+                        y: 28266
+                    },
+                     {
+                        x: new Date(2020, 2, 11),
+                        y: 45134
                     },
                     {
-                        x: new Date(2017, 0, 5),
-                        y: 710
+                        x: new Date(2020, 2, 21),
+                        y: 77675
                     },
-                    {
-                        x: new Date(2017, 0, 6),
-                        y: 658
+                     {
+                        x: new Date(2020, 3, 7),
+                        y: 106099
                     },
-                    {
-                        x: new Date(2017, 0, 7),
-                        y: 734
+                     {
+                        x: new Date(2020, 3, 16),
+                        y: 182414
                     },
-                    {
-                        x: new Date(2017, 0, 8),
-                        y: 963
+                     {
+                        x: new Date(2020, 3, 24),
+                        y: 422574
                     },
-                    {
-                        x: new Date(2017, 0, 9),
-                        y: 847
+                     {
+                        x: new Date(2020, 3, 28),
+                        y: 663127
                     },
-                    {
-                        x: new Date(2017, 0, 10),
-                        y: 853
+                     {
+                        x: new Date(2020, 3, 31),
+                        y: 858355
                     },
-                    {
-                        x: new Date(2017, 0, 11),
-                        y: 869
-                    },
-                    {
-                        x: new Date(2017, 0, 12),
-                        y: 943
-                    },
-                    {
-                        x: new Date(2017, 0, 13),
-                        y: 970
-                    },
-                    {
-                        x: new Date(2017, 0, 14),
-                        y: 869
-                    },
-                    {
-                        x: new Date(2017, 0, 15),
-                        y: 890
-                    },
-                    {
-                        x: new Date(2017, 0, 16),
-                        y: 930
-                    },
-                    {
-                        x: new Date(2017, 0, 17),
-                        y: 1850
-                    },
-                    {
-                        x: new Date(2017, 0, 18),
-                        y: 1905
-                    },
-                    {
-                        x: new Date(2017, 0, 19),
-                        y: 1980
-                    },
-                    {
-                        x: new Date(2017, 0, 20),
-                        y: 1858
-                    },
-                    {
-                        x: new Date(2017, 0, 21),
-                        y: 1034
-                    },
-                    {
-                        x: new Date(2017, 0, 22),
-                        y: 963
-                    },
-                    {
-                        x: new Date(2017, 0, 23),
-                        y: 847
-                    },
-                    {
-                        x: new Date(2017, 0, 24),
-                        y: 853
-                    },
-                    {
-                        x: new Date(2017, 0, 25),
-                        y: 869
-                    },
-                    {
-                        x: new Date(2017, 0, 26),
-                        y: 943
-                    },
-                    {
-                        x: new Date(2017, 0, 27),
-                        y: 970
-                    },
-                    {
-                        x: new Date(2017, 0, 28),
-                        y: 869
-                    },
-                    {
-                        x: new Date(2017, 0, 29),
-                        y: 890
-                    },
-                    {
-                        x: new Date(2017, 0, 30),
-                        y: 930
-                    },
-                    {
-                        x: new Date(2017, 0, 31),
-                        y: 750
-                    }
                 ]
             }]
         });
